2e5534fa2f3c2bc2e4bfd390bcc0add003db18cb
Yehor Hyrzhev <yehor.hyrzhev@student.tuke.sk>
2025-07-29 15:37:26 +0200

    commit2

--- app/main.py
+++ app/main.py
@@ -31,8 +31,10 @@ class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
         if coords is None:
             coords = [0, 0, 0]
-        elif len(coords) == 2:
-            coords.append(0)
+        else:
+            coords = coords.copy()
+            if len(coords) == 2:
+                coords.append(0)
         super().__init__(name, weight, coords)
 
     def go_up(self, step: int = 1) -> None:
